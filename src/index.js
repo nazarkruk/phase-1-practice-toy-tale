efe5f2d2df7b735898ca4092480773cd0defe7ab
Nazar Kruk <nazarkruk@icloud.com>
2022-02-22 22:21:26 -0500

    Done

--- src/index.js
+++ src/index.js
@@ -11,38 +11,11 @@ document.addEventListener("DOMContentLoaded", () => {
     } else {
       toyFormContainer.style.display = "none";
     }
-  });
-
-  renderEnimals()
-
-  function renderEnimals(){
-    fetch(' http://localhost:3000/toys')
-    .then(res=>res.json())
-    .then(array=> {
-      array.forEach(card=>{
-      let cardToy = document.createElement('div')
-      cardToy.innerHTML = `
-      <div class="card">
-      <h2>'${card['name']}'</h2>
-      <img src="${card['image']}" class="toy-avatar" />
-      <p>${card['likes']} Likes</p>
-      <button class="like-btn" id="${card["id"]}">Like ❤️</button>
-    </div>
-      `
-      document.querySelector('#toy-collection').appendChild(cardToy)
+  }) 
   
-      })
-    })
-
-  }
-
-
-  
-  
-  //add new toy
   let toyForm = document.querySelector('.add-toy-form')
   toyForm.addEventListener('submit', addNewToy)     
-
+  
   function addNewToy(e){
     e.preventDefault()
     let toyName = e.target.name.value
@@ -57,13 +30,58 @@ document.addEventListener("DOMContentLoaded", () => {
         name:toyName,
         image: toyImage,
         likes:0
-
       })
-
-    }) 
-    renderEnimals()
-
+    })
+    .then(res => res.json())
+    .then(card => renderToy(card)) 
   }
 
- 
 })
+
+
+getAllToys()
+
+function getAllToys(){
+  fetch(' http://localhost:3000/toys')
+  .then(res=>res.json())
+  .then(toyArray=> {toyArray.forEach(card=>renderToy(card))})
+}
+
+
+function renderToy(card){
+  let toyCard = document.createElement('div')
+      toyCard.innerHTML = `
+      <div class="card">
+      <h2>'${card.name}'</h2>
+      <img src="${card['image']}" class="toy-avatar" />
+      <p>
+        <span class = 'like-count'>${card.likes}</span> Likes
+      </p>
+      <button class="like-btn" id="${card.id}">Like ❤️</button>
+    </div>
+      `
+      let likeBtn = toyCard.querySelector('.like-btn')
+      likeBtn.addEventListener('click', ()=>{
+        card.likes += 1
+        toyCard.querySelector('.like-count').textContent = card.likes
+        fetch(`http://localhost:3000/toys/${card.id}`,{
+          method: "PATCH",
+          headers:{
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            "likes": card.likes
+          })
+
+        })
+ 
+      })
+      document.querySelector('#toy-collection').appendChild(toyCard)
+}
+
+
+
+
+
+
+
